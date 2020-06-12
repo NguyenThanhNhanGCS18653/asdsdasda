f858c0e6eaec63d5658b525255b660e1ad4fe786
thoconxinhxan <66501245+thoconxinhxan@users.noreply.github.com>
2020-06-12 15:26:10 +0900

    Add files via upload

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 
-define('DB_SERVER', 'ec2-184-73-209-230.compute-1.amazonaws.com');
-define('DB_USERNAME', 'mtycxjjccxmeee');
-define('DB_PASSWORD', 'f0dd25aaa62a3a7a83d3934915c681f6ddfac747814e5a3668945ea34017c6ce');
-define('DB_NAME', 'd35915da8p1v65');
+define('DB_SERVER', 'ec2-52-7-39-178.compute-1.amazonaws.com');
+define('DB_USERNAME', 'fqeyhzbqidlfyn');
+define('DB_PASSWORD', '5f4f096c464388de4fcc59c735e6605235b65804db900cc0f2c5b4b62e0039ec');
+define('DB_NAME', 'd8q21k88j6jf88');
  
 /* Attempt to connect to PostgreSQL database */
 $link = pg_connect("host=".DB_SERVER." dbname=". DB_NAME ." user=" . DB_USERNAME . " password=" .DB_PASSWORD. "")
